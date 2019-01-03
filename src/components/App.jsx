a79172b6e3a6ae4fbbf28dad02cb487c03ed83b2
abarena <abarena@tiempodevelopment.com>
2019-01-03 17:14:00 -0600

    lesson 20 trough 26

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class App extends Component {
+  static propTypes = {
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Loot Check</h2>
+      </div>
+    )
+  }
+}
