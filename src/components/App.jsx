86886504ddc7a28cda2c4816082f48fb074955e4
abarena <abarena@tiempodevelopment.com>
2019-01-04 18:12:48 -0600

    lesson 26 through 30

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import Wallet from './Wallet';
 
 export default class App extends Component {
-  static propTypes = {
-  }
 
   render() {
     return (
       <div>
         <h2>Loot Check</h2>
+        <hr />
+        <Wallet />
       </div>
     )
   }
---
+++ src/components/Wallet.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deposit } from '../actions/balance';
+export class Wallet extends Component {
+
+  state = {
+    balance: undefined
+  }
+
+  updateBalance = event => this.setState({balance: parseInt(event.target.value, 10)});
+  
+  deposit = () => this.props.deposit(this.state.balance);
+  render() {
+    return (
+      <div>
+        <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+        <br />
+        <input className='input-wallet' onChange={this.updateBalance}/>
+        <button className='btn-deposit' onClick={this.deposit}>Deposit</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({balance: state});
+
+export default connect(mapStateToProps, {deposit})(Wallet);
